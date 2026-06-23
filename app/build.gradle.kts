7797700f8ca42ce006c021c636b17b8c0853ce4d
Gary Keeler <iamkeeler@gmail.com>
2026-06-23 14:04:15 -0500

    Use official M3 FlexibleBottomAppBar in Scaffold bottomBar slot for docked toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
   // Compose
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
+  implementation("androidx.compose.material3:material3:1.5.0-alpha01")
   implementation("androidx.compose.material:material-icons-core")
   // Tooling
   debugImplementation(libs.androidx.compose.ui.tooling)
