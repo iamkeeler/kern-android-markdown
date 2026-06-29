fa935ff4fe523eccccf6c6f1db0d55507080a8d4
Gary Keeler <iamkeeler@gmail.com>
2026-06-28 19:00:32 -0500

    Fix unresolved reference BorderStroke after merge

--- app/src/main/java/com/attachdesign/kern/ui/main/MainScreen.kt
+++ app/src/main/java/com/attachdesign/kern/ui/main/MainScreen.kt
@@ -2,6 +2,7 @@ package com.attachdesign.kern.ui.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
